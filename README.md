dfd43957776daf164f1ff62cb79c2932f7c8771e
Ademola Adegbuyi <ooade96@gmail.com>
2017-10-08 18:45:26 +0100

    Update Readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Simple Todo
 
 > A Simple FireStore implementation with Vuex
+
+> [View App](https://vuex-firestore.surge.sh)
--- store/firestore.js
+++ store/firestore.js
@@ -1,37 +1,35 @@
-import firebase from 'firebase';
-require('firebase/firestore');
-import store from './';
-
-console.log(process.env.HITME);
+import firebase from 'firebase'
+require('firebase/firestore')
+import store from './'
 
 firebase.initializeApp({
 	apiKey: 'ENTER_API_KEY',
 	authDomain: 'ENTER_AUTH_DOMAIN',
 	projectId: 'ENTER_PROJECT_ID'
-});
+})
 
-const db = firebase.firestore();
-const todos = db.collection('todos');
+const db = firebase.firestore()
+const todos = db.collection('todos')
 
 // Getting Real time feeds
 todos.onSnapshot(querySnapshot => {
-	const myTodos = [];
+	const myTodos = []
 	querySnapshot.forEach(doc => {
-		myTodos.push({ id: doc.id, ...doc.data() });
-	});
-	store.commit('watchTodos', myTodos);
-});
+		myTodos.push({ id: doc.id, ...doc.data() })
+	})
+	store.commit('watchTodos', myTodos)
+})
 
 export default {
 	fetchTodos: () => {
-		return todos.get();
+		return todos.get()
 	},
 
 	addTodo: text => {
-		return todos.add({ text });
+		return todos.add({ text })
 	},
 
 	removeTodo: id => {
-		return todos.doc(id).delete();
+		return todos.doc(id).delete()
 	}
-};
+}
